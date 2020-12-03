2070bcb4fb69635b2d77ae149eec5403e740b0c1
ilya <ikuraev88@gmail.com>
2020-12-03 13:28:00 +0000

    Initial commit

---
+++ src/main/java/com/example/ibs/IbsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ibs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ibs/IbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ibs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
